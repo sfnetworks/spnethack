506086a95fbebfd351de5c15439bcd02f37aa4c7
loreabad6 <lore.abad6@gmail.com>
2019-01-07 16:23:35 +0100

    update with dodgr

--- dodgr-promenade.R
+++ dodgr-promenade.R
@@ -6,8 +6,8 @@ promenade_graph = weight_streetnet(promenade_dodgr, wt_profile = 'bicycle')
 # promenade_vert = dodgr_vertices(promenade_graph)
 
 ## Sample points along the route to create flows
-from = sample(promenade_graph$from_id, size = 100)
-to = sample(promenade_graph$to_id, size = 100)
+from = sample(promenade_graph$from_id, size = 1000)
+to = sample(promenade_graph$to_id, size = 1000)
 flows = matrix(10*runif(
   length(from)*length(to)),
   nrow = length (from)
