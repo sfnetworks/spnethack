b44818813b1bf79c47d4283f3af28645941baa8e
loreabad6 <lore.abad6@gmail.com>
2019-01-07 17:26:24 +0100

    delete extra lines

--- stplanr-promenade.R
+++ stplanr-promenade.R
@@ -2,9 +2,3 @@ rstp = SpatialLinesNetwork(sl = promenade_min)
 rstp@sl$b = igraph::edge.betweenness(rstp@g)
 lwd = rstp@sl$b / mean(rstp@sl$b) * 5
 plot(rstp, lwd = lwd)
-
-rtg = as_tbl_graph(x = rstp@g)
-
-rtg_edges = activate(rtg,edges) %>% mutate(geometry = promenade_min$geometry)
-
-st_as_sf(as.tibble(rtg_edges)) 
